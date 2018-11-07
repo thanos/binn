15a0b57b94dd481ddb5876d0e9050b56815389a8
thanos <thanosv@gmail.com>
2018-11-07 10:29:28 -0500

    updated mix

--- mix.exs
+++ mix.exs
@@ -17,10 +17,18 @@ defmodule Binn.MixProject do
       extra_applications: [:logger]
     ]
   end
+  
+  defp package() do
+    [maintainers: ["Thanos VAssilakis"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/thanos/binn"}]
+  end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false},
+     {:plug, "~> 1.0", optional: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
